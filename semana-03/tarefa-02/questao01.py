49bbf04c4206a9636ebd1d5b82a5eb2e992c68fe
Isaac Alves <isaacfelipee3@gmail.com>
2024-11-01 20:39:00 -0300

    F.

---
+++ semana-03/tarefa-02/questao01.py
@@ -0,0 +1,13 @@
+PI = 3.14
+
+raio = float(input())
+
+circunferencia = 2 * PI * raio
+areaCirculo = PI * raio**2
+areaEsfera = 4 * PI * raio ** 2
+volumeEsfera = (4 * PI * raio ** 3) / 3
+
+print(circunferencia)
+print(areaCirculo)
+print(areaEsfera)
+print(volumeEsfera)
