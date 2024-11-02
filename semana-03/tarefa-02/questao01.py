2f0c390b801853cee4e96414983d763f6d692ede
Isaac Alves <isaacfelipee3@gmail.com>
2024-11-01 21:04:56 -0300

    .

--- semana-03/tarefa-02/questao01.py
+++ semana-03/tarefa-02/questao01.py
@@ -1,13 +1,13 @@
-PI = 3.14
+PI = 3.141592
 
-raio = float(input())
+raio = float(input("Digite o raio do círculo: "))
 
 circunferencia = 2 * PI * raio
-areaCirculo = PI * raio**2
+areaCirculo = PI * raio ** 2
 areaEsfera = 4 * PI * raio ** 2
 volumeEsfera = (4 * PI * raio ** 3) / 3
 
-print(circunferencia)
-print(areaCirculo)
-print(areaEsfera)
-print(volumeEsfera)
+print(f"Esse é o comprimento da circunferencia: {circunferencia:.6f}")
+print(f"Essa é a área do círculo: {areaCirculo:.6f}")
+print(f"Essa é a área da esfera: {areaEsfera:.6f}")
+print(f"Esse e o volume da esfera: {volumeEsfera:.6f}")
