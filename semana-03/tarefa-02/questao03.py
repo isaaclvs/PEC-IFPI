dfb1255794811ccd445ac5713af9c720cd8410fb
Isaac Alves <isaacfelipee3@gmail.com>
2024-11-01 21:39:02 -0300

    .

---
+++ semana-03/tarefa-02/questao03.py
@@ -0,0 +1,5 @@
+dividendo = float(input("Digite o número a ser dividido: "))
+divisor = float(input("Digite por quanto você quer dividir: "))
+
+print(f"O resultado da divisão é: {(dividendo // divisor):.4f}")
+print(f"O resto da divisão é: {(dividendo % divisor):.4f}")
