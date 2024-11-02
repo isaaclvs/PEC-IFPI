dcd551b2dfd949efdb935e76d87392232a53948d
Isaac <isaacfelipee3@gmail.com>
2024-11-02 16:17:15 -0300

    Final project

---
+++ semana-03/ano-2025/passo-03/desafio-qualquer-ano.py
@@ -0,0 +1,5 @@
+nascimento = int(input("Em que ano você nasceu? "))
+ano = int(input("Para qual ano você quer saber? "))
+
+idadeFutura = ano - nascimento
+print(f"Em {ano} você terá {idadeFutura} anos!")
---
+++ semana-03/ano-2025/passo-03/idade-ano-cachorro.py
@@ -0,0 +1,3 @@
+idade = int(input("Qual a sua idade? "))
+idadeCachorro = idade * 7
+print(f"Se você fosse um cachorro você teria {idadeCachorro} anos")
