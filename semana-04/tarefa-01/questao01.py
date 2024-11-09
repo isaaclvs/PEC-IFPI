16651a2ada3ce2a662ac8e39a1971f275fa8d8ea
Isaac <isaacfelipee3@gmail.com>
2024-11-09 17:23:20 -0300

    .

--- semana-04/tarefa-01/questao01.py
+++ semana-04/tarefa-01/questao01.py
@@ -1,5 +1,4 @@
-dia = input()
-mes = input()
-ano = input()
-
+dia = input("Digite o dia: ").lstrip("0").strip()
+mes = input("Digite o mes: ").lstrip("0").strip()
+ano = input("Digite o ano: ").lstrip("0").strip()
 print(f"{dia}/{mes}/{ano}")
