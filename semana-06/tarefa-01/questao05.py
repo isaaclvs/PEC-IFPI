73e15be47a0303ccf3230e3a03439da8b011326c
Isaac <isaacfelipee3@gmail.com>
2024-12-03 15:30:34 -0300

    .

--- semana-06/tarefa-01/questao05.py
+++ semana-06/tarefa-01/questao05.py
@@ -1,4 +1,4 @@
-valorCompra = float(input())
+valorCompra = float(input("Digite o valor da compra: "))
 
 valorAvista = valorCompra - (9/100 * valorCompra)
 
@@ -6,6 +6,6 @@ valorPrestacao5x = valorCompra / 5
 
 valorPrestacao10x = (valorCompra + (17/100 * valorCompra)) / 10
 
-print(f"{valorAvista:.2f}")
-print(f"{valorPrestacao5x:.2f}")
-print(f"{valorPrestacao10x:.2f}")
+print(f"Valor a vista: {valorAvista:.2f}")
+print(f"Valor em 5x: {valorPrestacao5x:.2f}")
+print(f"Valor em 10x: {valorPrestacao10x:.2f}")
